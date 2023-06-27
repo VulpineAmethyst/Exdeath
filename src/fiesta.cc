4f793c02b42ae02bdf98cb3ae0e8e979606acc2f
Síle Ekaterin Liszka <sheila@vulpine.house>
2023-06-27 07:31:02 -0700

    update names and copyrights

--- src/fiesta.cc
+++ src/fiesta.cc
@@ -1,10 +1,9 @@
 /*
- * Copyright (c) 202BER, Sheila Aman.
+ * Copyright (c) 2023, Síle Ekaterin Liszka.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
  *    * Redistributions of source code must retain the above copyright
  *      notice, this list of conditions and the following disclaimer.
  *    * Redistributions in binary form must reproduce the above copyright
--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, Sheila Aman.
+ * Copyright (c) 2023, Síle Ekaterin Liszka.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
